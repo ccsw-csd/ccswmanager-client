eafdbb400f880fb209feb97b93a2d8970fd8f0d3
Armen <100298950+armirzoya@users.noreply.github.com>
2022-03-02 14:42:39 +0100

    Fix chart build error

--- src/app/scholar/timeline-dialog/timeline-dialog.component.ts
+++ src/app/scholar/timeline-dialog/timeline-dialog.component.ts
@@ -44,57 +44,12 @@ export class TimelineDialogComponent implements OnInit {
   
   constructor(private scholarService: ScholarService) {
     this.dateRange();
-    //this.getScholarsByDate();
 
     this.chartOptions = {
       series: [
         {
-          //data: this.scholarData,
-          
-          data: [/*
-            {
-              x: 'Analysis',
-              y: [
-                new Date("2019-02-27").getTime(),
-                new Date("2019-03-04").getTime()
-              ],
-              fillColor: '#008FFB'
-            },
-            {
-              x: "Design",
-              y: [
-                new Date("2019-03-04").getTime(),
-                new Date("2019-03-08").getTime()
-              ],
-              fillColor: "#00E396"
-            },
-            {
-              x: "Coding",
-              y: [
-                new Date("2019-03-07").getTime(),
-                new Date("2019-03-10").getTime()
-              ],
-              fillColor: "#775DD0"
-            },
-            {
-              x: "Testing",
-              y: [
-                new Date("2019-03-08").getTime(),
-                new Date("2019-03-12").getTime()
-              ],
-              fillColor: "#FEB019"
-            },
-            {
-              x: "Deployment",
-              y: [
-                new Date("2019-03-12").getTime(),
-                new Date("2019-03-17").getTime()
-              ],
-              fillColor: "#FF4560"
-            },*/
-          ]
+          data: []
         }
-        
       ],
       chart: {
         height: 350,
@@ -122,13 +77,10 @@ export class TimelineDialogComponent implements OnInit {
         }
       }
     };
-    //this.getScholarsByDate();
   }
   
   ngOnInit(): void {
-    //this.getScholars();
     this.getScholarsByDate();
-    //debugger;
   }
 
   dateRange(){
@@ -139,57 +91,45 @@ export class TimelineDialogComponent implements OnInit {
     this.minDate = new Date(currentYear, currentMonth - 2, currentDay);
     this.maxDate = new Date(currentYear, currentMonth + 12, currentDay);
   }
-  /*
-  getScholars(){
-    this.scholarService.findScholars().subscribe( (data) => {
-      this.scholarData = data;
-      debugger;
-    }); 
-  }
-  */
+
   getScholarsByDate(){
     this.scholarService.findScholarsByDate(this.startDate, this.endDate).subscribe( (res) => {
       this.scholarData = res;
-      //let result = []
 
-      let date = new Date("2019-03-04").getTime();
-      /*
-      this.scholarData.forEach((res1) =>
-      result.push({
-          'x': res1.x,
-          'y': res1.y?.forEach((res2) =>
-            res2 = new Date(new Date(res2).getTime()),
-          ),
-          'fillColor': res1.fillColor
-        })
-      )
-      */
-     /*
+      //TODO limpiar todo esto
       this.chartOptions = {
         series: [
           {
-            data: {
-              fillColor: "#00E396",
-              x: "asolerpa",
-              y: [1645311600000, 1647730800000]
-            },
+            data: res
           }
-        ]
-      }
-      */
-
-      
-      this.chart?.updateSeries([{
-        data: [{
-          fillColor: "#00E396",
-          x: "asolerpa",
-          y: [1645311600000, 1647730800000]
-        },]
-      }])
-      
-
-      debugger;
-    }); 
+        ],
+        chart: {
+          height: 350,
+          type: "rangeBar"
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            distributed: true,
+            dataLabels: {
+              hideOverflowingLabels: false
+            }
+          }
+        },
+        xaxis: {
+          type: "datetime"
+        },
+        yaxis: {
+          show: true
+        },
+        grid: {
+          row: {
+            colors: ["#f3f4f5", "#fff"],
+            opacity: 1
+          }
+        }
+      };
+    });
   }
   
 }
