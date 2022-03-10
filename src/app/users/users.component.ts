7bf7e32c05e95d3404ebb4d834f3cc6cf51b1255
aelmouss <ayoub.el-moussaoui-achibani-external@capgemini.com>
2022-03-10 12:53:00 +0100

    Se ha establicido el tamaño. Dependiendo de la columna tendrá un tamaño u otro.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,7 +12,8 @@ import { CustomCellButtonComponent } from './custom-cell-button/custom-cell-butt
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  
+  tooltipShowDelay = 0;
+  tooltipHideDelay = 2000;
   api: GridApi = new GridApi;
   usuarios: Usuario[] = [];
   allUsername !: string[];
@@ -32,14 +33,14 @@ export class UsersComponent implements OnInit {
 
     columnDefSch: ColDef[] = 
     [
-      { field: 'username', headerName: 'Nombre de usuario', sortable: true, cellStyle: {'background-color': '#F8F8F8'}},
-      { field: 'name', headerName: 'Nombre', sortable: true, cellStyle: {'background-color': '#F8F8F8'}}, 
-      { field: 'lastname', headerName: 'Apellidos', sortable: true, cellStyle: {'background-color': '#F8F8F8'}}, 
-      { field: 'role', headerName: 'Rol', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, width: 264},
-      { field: 'customers', headerName: 'Equipos', sortable: true, cellStyle: {'background-color': '#F8F8F8'}},
+      { field: 'username', headerName: 'Nombre de usuario', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, maxWidth: 160, minWidth: 130},
+      { field: 'name', headerName: 'Nombre', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, maxWidth: 230, minWidth : 130, tooltipField: 'name'}, 
+      { field: 'lastname', headerName: 'Apellidos', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, maxWidth: 320, minWidth: 180, tooltipField: 'lastname'}, 
+      { field: 'role', headerName: 'Rol', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, maxWidth: 100, minWidth: 90},
+      { field: 'customers', headerName: 'Equipos', sortable: true, cellStyle: {'background-color': '#F8F8F8'}, minWidth: 200, tooltipField: 'customers'},
       { headerName: 'Acción', cellRenderer: 'btnCellRenderer', cellRendererParams: {
         onClick: this.onBtnClick.bind(this)
-      },cellStyle: {'background-color': '#F8F8F8'}, filter : false}
+      },cellStyle: {'background-color': '#F8F8F8'}, filter : false, maxWidth: 120, minWidth: 100}
     ]
         
   ngOnInit(): void {
@@ -54,7 +55,7 @@ export class UsersComponent implements OnInit {
       },
       floatingFilterComponentParams: {suppressFilterButton:true},
       suppressMenu:true,
-      singleClickEdit: true
+      singleClickEdit: true,
     };
   }
 
