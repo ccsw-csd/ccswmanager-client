fc20a826769fae72ef6ca71f19ebc5ed7f07b952
CORP\jchengli <juan-juanxin.cheng-liu-external@capgemini.com>
2022-03-16 09:46:06 +0100

    creacion de la pantalla pyramid 2

---
+++ src/app/pyramid/pyramid.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PyramidComponent } from './pyramid.component';
+
+describe('PyramidComponent', () => {
+  let component: PyramidComponent;
+  let fixture: ComponentFixture<PyramidComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PyramidComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PyramidComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
